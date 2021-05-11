fe013db550996c0649b9264a406e48039f4ac577
AdamsSanyeRahinatu <rahinatuadams19@gmail.com>
2021-05-11 10:21:37 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form-cart");
+const itemContainer = document.getElementById("item-container");
+
+//submit handler or function
+function addItem(e) {
+  e.preventDefault();
+  let li = document.createElement("li");
+  li.textContent = form.item_name.value;
+  itemContainer.appendChild(li);
+};
+
+addEventListener("submit", addItem);
